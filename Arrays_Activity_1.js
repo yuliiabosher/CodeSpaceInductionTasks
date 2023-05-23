0c8c7111013f5f0fa7ff3f11929801485a6bb101
yuliiabosher <128724492+yuliiabosher@users.noreply.github.com>
2023-05-23 15:00:50 +0100

    Update Arrays_Activity_1.js

--- Arrays_Activity_1.js
+++ Arrays_Activity_1.js
@@ -1,6 +1,6 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let sum = 0;
-for (i=0; i<array.length; i++) {
-	sum += array[i]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // declared and initialised an array
+let sum = 0;					// declared and initialised a sum variable 
+for (i=0; i<array.length; i++) {		// created a loop to iterate through array 
+	sum += array[i]				// sum incremented with each array element
 }
-console.log(sum)
+console.log(sum)				// output sum
