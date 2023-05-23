0203b25d650f0a289df44a20e0b834203afe2eb9
yuliiabosher <128724492+yuliiabosher@users.noreply.github.com>
2023-05-23 11:06:10 +0100

    added 2 files with Activity 1 task from the JS Loops tutorial

---
+++ Loops_Activity_1.js
@@ -0,0 +1,10 @@
+for (i=0; i<=15; i++) {				// created a loop that iterates from 0 to 15
+	if (i%2 == 0) {				// check if the remainder of the given number's division by 2 is 0
+		console.log(`${i} is even`);	// if true output that the given number is even
+	} else {				
+		console.log(`${i} is odd`);	// if false output that the given number is odd
+	}
+	
+}
+
+
