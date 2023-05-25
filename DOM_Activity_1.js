bfee221fa8b0b885c6040ccd3bdb229fcddf356e
yuliiabosher <128724492+yuliiabosher@users.noreply.github.com>
2023-05-25 12:20:25 +0100

    added 2 files Activity 1 JS DOM

---
+++ DOM_Activity_1.js
@@ -0,0 +1,3 @@
+let x = document.getElementById("text");   // declared and initialised a variable x that finds the p tag 
+x.style.backgroundColor = "blue";	// changed the CSS style background property of the object
+x.style.color = "red";			// changed the text colour property
