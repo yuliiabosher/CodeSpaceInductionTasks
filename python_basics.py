54e7f2f38c9d0e69d2fc4b408f6f2839b138b393
yuliiabosher <128724492+yuliiabosher@users.noreply.github.com>
2023-06-01 15:38:41 +0100

    Python Basics Activity 1

---
+++ python_basics.py
@@ -0,0 +1,8 @@
+a = -5+8*6 #assign value to a new variable a
+print(a) # output a
+b = (55+9)%9 # assign value in a form of mathematical operation to a new variable b
+print(b) # output b
+c = 20+-3*5/8  # assign value to a new variable c
+print(c)  # output c
+d = 5+15/3*2-8%3 # assign value to new variable d 
+print(d)  #output d
