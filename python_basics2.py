5f5cc63e36e7342cc1d0d4f47b1548f8dd04a7a2
yuliiabosher <128724492+yuliiabosher@users.noreply.github.com>
2023-06-01 15:50:01 +0100

    Python Basics Activity 2

---
+++ python_basics2.py
@@ -0,0 +1,4 @@
+x = int(input("Input the first number: "))  #create a variable that will take string input and turn it into an integer
+y = int(input("Input the second number: ")) #create a second variable that takes input and turns it into an integer
+z = str(int(x/y)) # create a variable that will store the value which is the result of the division of x by y, turn it into an integer and then into a string
+print("The division of the first number and the second number is: "+z)   # output a string and the value of z variable concatenated to it
