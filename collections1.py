2a1f1587e56f363b9f11d02401dec2685596fba7
yuliiabosher <128724492+yuliiabosher@users.noreply.github.com>
2023-06-02 16:00:52 +0100

    Python Collections Activity 1

---
+++ collections1.py
@@ -0,0 +1,5 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]	# assign a variable with the value our the sample list
+sum = 0					# assign a value of 0 to a new variable that will be used to calculate the sum
+for x in list:				# create a for loop to parse through the values of a list
+	sum += x			# add each value of a list to the sum variable
+print(sum)				# print the final sum variable
